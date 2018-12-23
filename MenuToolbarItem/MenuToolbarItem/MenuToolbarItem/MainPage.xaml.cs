f0799a5610db97c9fa560ccee20b9a4646c8aa78
marcellm <marcell.marchetto@gmail.com>
2018-12-22 23:16:34 -0200

    OK

---
+++ MenuToolbarItem/MenuToolbarItem/MenuToolbarItem/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MenuToolbarItem
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, true);
+        }
+
+        private void PesquisarClicked(object sender, EventArgs args)
+        {
+            DisplayAlert("Alerta", "Pesquisando...", "OK");
+        }
+
+        private void BaixarClicked(object sender, EventArgs args)
+        {
+            DisplayAlert("Alerta", "Baixando...", "OK");
+        }
+
+        private void EstornarClicked(object sender, EventArgs args)
+        {
+            DisplayAlert("Alerta", "Estornando...", "OK");
+        }
+
+        private void ExcluirClicked(object sender, EventArgs args)
+        {
+            DisplayAlert("Alerta", "Excluindo...", "OK");
+        }
+    }
+}
